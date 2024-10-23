4124cf42f6607eefd886de99a3e079daf3f00800
mohammedateeef <atefm.me556@gmail.com>
2024-10-23 12:18:29 +0300

    changes

--- routes/blogsRoutes.js
+++ routes/blogsRoutes.js
@@ -7,7 +7,7 @@ const cacheMiddleware = require('../middlewares/cachingMiddleware');
 
 const router = express.Router();
 
-router.get("/get-blog/:id", cacheMiddleware((req) => `blog_${req.params.id}`, 120), getBlog);
-router.get("/get-all-blogs", cacheMiddleware(() => 'all_blogs', 120), getAllBlogs);
+router.get("/get-blog/:id", cacheMiddleware((req) => `blog_${req.params.id}`, 86400), getBlog);
+router.get("/get-all-blogs", cacheMiddleware(() => 'all_blogs', 86400), getAllBlogs);
 
 module.exports = router;
